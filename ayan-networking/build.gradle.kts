04dd1fa4adf3feddaf3de6948ad414fa63e452b1
ali tafreshi <alitafreshi123@yahoo.com>
2022-10-01 13:22:44 +0330

    Done

--- ayan-networking/build.gradle.kts
+++ ayan-networking/build.gradle.kts
@@ -21,3 +21,26 @@ dependencies {
     //Data store
     implementation(DataStore.data_store)
 }
+
+publishing {
+    publications {
+        // Creates a Maven publication called "release".
+        register("release", MavenPublication::class) {
+            // Applies the component for the release build variant.
+            // NOTE : Delete this line code if you publish Native Java / Kotlin Library
+//            from(components["release"])
+            from(components.getByName("java"))
+
+            // Library Package Name (Example : "com.frogobox.androidfirstlib")
+            // NOTE : Different GroupId For Each Library / Module, So That Each Library Is Not Overwritten
+            groupId = "com.github.alitafreshi"
+
+            // Library Name / Module Name (Example : "androidfirstlib")
+            // NOTE : Different ArtifactId For Each Library / Module, So That Each Library Is Not Overwritten
+            artifactId = "ayan-networking"
+
+            // Version Library Name (Example : "1.0.0")
+            version = "0.0.1"
+        }
+    }
+}
