0dd8809cbcb24c4c72c19954b468bf3fc0f55e5a
ali tafreshi <alitafreshi123@yahoo.com>
2022-10-06 21:21:52 +0330

    FIX

--- app/src/main/java/com/alitafreshi/ayannewnetworking/MainActivity.kt
+++ app/src/main/java/com/alitafreshi/ayannewnetworking/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.alitafreshi.ayannewnetworking
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.alitafreshi.ayannewnetworking.ui.theme.AyanNewNetworkingTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,22 +20,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AyanNewNetworkingTheme {
-        Greeting("Android")
     }
 }
