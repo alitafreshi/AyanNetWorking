7dca364a8b104c609258517a8fb01cd6a498a520
ali tafreshi <alitafreshi123@yahoo.com>
2022-10-06 21:22:31 +0330

    FIX

--- ayan-networking/build.gradle.kts
+++ ayan-networking/build.gradle.kts
@@ -43,7 +43,7 @@ publishing {
             artifactId = "ayan-networking"
 
             // Version Library Name (Example : "1.0.0")
-            version = "0.0.12"
+            version = "0.0.13"
         }
     }
 }
